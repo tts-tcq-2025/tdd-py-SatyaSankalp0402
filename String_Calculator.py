8e249e6325e9c3d79981e7bcb8103b3274b59c9d
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-22 16:30:34 +0530

    Update String_Calculator.py

--- String_Calculator.py
+++ String_Calculator.py
@@ -1,8 +1,14 @@
 def add(numbers):
   if numbers == "":
     return 0
-  newnumbers = numbers.replace("\n", ",")
-  parts = newnumbers.split(",")
+  delimiters = [",", "\n"]
+  if numbers.startswith("//"):
+    delimiter_line, newnumbers = numbers.split("\n", 1)
+    custom_delimiter = delimiter_line[2:]  # skip //
+    delimiters = [custom_delimiter]
+  for index in delimeters:
+    actualnumbers=newnumbers.replace(index,",")
+  parts = actualnumbers.split(",")
   return sum(int(x) for x in parts)
   
 
