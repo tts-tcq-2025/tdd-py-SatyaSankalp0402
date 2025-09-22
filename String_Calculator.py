4f62d0d799b220b874d5a31c4f8fafffcde20f18
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-22 15:58:38 +0530

    Update String_Calculator.py

--- String_Calculator.py
+++ String_Calculator.py
@@ -1,6 +1,10 @@
 def add(numbers):
   if numbers == "":
     return 0
+  result=sum_multiple_numbers
+  return result
+  return int(numbers)
+
+def sum_multiple_numbers(numbers):
   if "," in numbers:
     return sum(int(parts) for parts in numbers.split(","))
-  return int(numbers)
