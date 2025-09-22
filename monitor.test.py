c1592b05c044c2717528f0700b1b6c7ae7083ef2
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-22 15:41:27 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -7,7 +7,7 @@ class StringCalculatorTests(unittest.TestCase):
   def test_single_number_returns_its_value(self):
     self.assertEqual(add("1"), 1)
   def test_two_numbers_comma_separated_returns_sum(self):
-    self.assertEqual(add("1,2", 3)
+    self.assertEqual(add("1,2"), 3)
 
 if __name__ == "__main__":
   unittest.main()
