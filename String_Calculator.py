e070a0d96fecabd3111cfd1347c49c0bb16307e5
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-23 09:36:09 +0530

    Update String_Calculator.py

--- String_Calculator.py
+++ String_Calculator.py
@@ -1,15 +1,32 @@
 def add(numbers):
   if numbers == "":
     return 0
+  numbers, delimiter = extract_delimeter(numbers)
+  tokens= split_numbers(numbers,delimeter)
+  values= integer_converter(tokens)
+  check_negatives(values)
+  return sum(values)
+
+def extract_delimeter(numbers):
   if numbers.startswith("//"):
     delimiter_line, numbers = numbers.split("\n", 1)
-    delimiter = delimiter_line[2:]
+    return numbers, delimiter_line[2:]
+  return numbers, ","
+
+def split_numbers(numbers,delimeter):
+  numbers = numbers.replace("\n", ",")
+  if delimeter != ",":
     numbers = numbers.replace(delimiter, ",")
-  else:
-    numbers = numbers.replace("\n", ",")
-  parts=[int(x) for x in numbers.split(",") if x]
-  negatives=[x for x in parts if x<0]
+  return [x for x in numbers.split(",") if x]
+
+def integer_converter(tokens):
+  return [int(x) for x in tokens]
+
+def check_negatives(values):
+  negatives = [x for x in values if x<0]
   if negatives:
     raise ValueError(f"negatives not allowed:{negatives}")
-  return sum(x for x in parts if x<=1000)
 
+def sum(values):
+  return sum(x for x in values if x<=1000)
+  
